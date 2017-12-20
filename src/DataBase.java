d38ad736a1bf31ee20455dadbed3c40183d63289
Unknown <ab.barnawi@gmail.com>
2017-12-20 17:29:20 +0300

    Added DataBase
    DataBase Is Not complete Yet

---
+++ src/DataBase.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+
+public class DataBase {
+	ArrayList<Car> cars;
+	ArrayList<Account> accounts;
+	ArrayList<Bill> bills;
+	
+	public DataBase(){
+		cars = new ArrayList<Car>();
+		accounts = new ArrayList<Account>();
+		bills = new ArrayList<Bill>();
+		
+		
+		
+	}
+	
+	public void readDatabaseFiles(){
+		// TO BE DONE LATER ! 
+	}
+
+}
