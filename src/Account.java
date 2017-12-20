08fdb3344168176a0b87e6e683f13ec9608fd33b
Amro <amrobahrawy@hotmail.com>
2017-12-20 17:36:17 +0300

    Bill implementation and getUsername for account
    added getter and setter for username and created bill class

--- src/Account.java
+++ src/Account.java
@@ -23,6 +23,12 @@ public Account(String username, String password, String adress,
 	statusCnt = 0;
 	
 }
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
 public void login(){
 	
 	
